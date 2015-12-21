d02fb74fdb1c48dc919b01bf5bcf2c9038e6b93d
Taylor Monacelli <taylor.monacelli@streambox.com>
2015-12-20 18:49:42 -0800

    Add stuff after running yo angular t1

---
+++ t1/app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name t1App
+ * @description
+ * # t1App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('t1App', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ t1/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name t1App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the t1App
+ */
+angular.module('t1App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
